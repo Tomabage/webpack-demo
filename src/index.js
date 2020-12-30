692e27015548be71500c09020304108accefa2da
Tomabage <tamaoona@163.com>
2020-12-30 14:53:36 +0800

    准备上传

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import x from "./x.js";
+import png from "./assets/1.png";
 
+const app2 = document.getElementById("app2");
+app2.innerHTML = `
+    <img src="${png}" alt="这是一个太阳">
+`;
 console.log(x);
-console.log("hi");
-console.log("不对吗");
+
+const app1 = document.getElementById("app1");
+const button = document.createElement("button");
+app1.appendChild(button);
+button.innerText = "懒加载";
+button.onclick = () => {
+  const promise = import("./lazy");
+  promise.then(
+    (module) => {
+      const fn = module.default;
+      fn();
+    },
+    () => {
+      console.log("模块加载错误");
+    }
+  );
+};
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -13,4 +13,66 @@ module.exports = {
       template: "src/assets/index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: "stylus-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "less-loader",
+            options: {
+              lessOptions: {
+                strictMath: true,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("dart-sass"),
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
